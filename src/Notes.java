6fe1446d078b7f6d9a38a11c4a3528c3c814704b
jose jimenez <jimenjos000@warren.k12.in.us>
2024-08-13 13:38:42 -0400

    asiignlkjn;

--- src/Notes.java
+++ src/Notes.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Notes {
+public class Notes {
+    public static void main(String[] args) {
+
+    }
+
+    public static void checkNumber(int number){
+        if(number > 0){
+            System.out.println("The number is positive");
+
+        } else if(number < 0) {
+            System.out.println("The number is negative");
+
+        }else{
+            System.out.println("zero");
+
+
+        }
+    }
+
+
 }
